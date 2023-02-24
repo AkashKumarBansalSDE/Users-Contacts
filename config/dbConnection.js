a4ca2e59d7470a45706d38721a74f58455523ffa
AkashKumarBansalSDE <bansalakash160@gmail.com>
2023-02-24 10:00:19 +0530

    updated

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -11,13 +11,3 @@ const connectDb = async ()=>{
 };
 
 module.exports = connectDb;  
-
-// const mongoose = require('mongoose');
-// // Replace <dbuser>, <dbpassword>, <dbname>, and <cluster> with your own database information 
-// const uri = "mongodb+srv://bansalakash160:vo6mA9OPyVwrRTPz@cluster0.5p59uek.mongodb.net/mycontacts-backend?retryWrites=true&w=majority";
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection; 
-// db.on('error', console.error.bind(console, 'connection error:')); 
-// db.once('open', function () { console.log('Connected to MongoDB database'); });
-
-// module.exports = db;
